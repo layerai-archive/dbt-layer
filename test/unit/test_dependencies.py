d5d6ffda8bb7d789da439d916fab1aebaf499bf4
Dimitar Popov <dimitar@layer.co>
2022-05-11 11:48:45 +0100

    Add test to ensure the dbt-core test dependency is correct

---
+++ test/unit/test_dependencies.py
@@ -0,0 +1,24 @@
+from distutils import core
+
+
+import pathlib
+import re
+import toml
+
+def test_dbt_core_version():
+    repo_root = pathlib.Path(__file__).parent.parent.parent
+
+    dbt_version_file = repo_root / "dbt" / "adapters" / "layer_bigquery" / "__version__.py"
+    with open(dbt_version_file) as f:
+        content = f.read()
+        match = re.search('version = "(.*)"', content)
+        dbt_version = match.group(1)
+
+    pyproject_file = repo_root / "pyproject.toml"
+
+    with open(pyproject_file) as f:
+        pyproject = toml.load(f)
+        dbt_core_version = pyproject['tool']['poetry']['dependencies']['dbt-core']
+
+    assert dbt_core_version == dbt_version
+
