94827470cbb8554f99781f68eab02833259b4443
maasg <gerard.maas@gmail.com>
2022-05-27 13:25:26 +0200

    Format

--- common/adapter.py
+++ common/adapter.py
@@ -117,7 +117,6 @@ class LayerAdapter(BaseAdapter):  # pylint: disable=abstract-method
         else:
             raise RuntimeException(f'Unknown layer function "{layer_sql_function.function_type}"')
 
-
     def _run_layer_train(
         self,
         layer_sql_function: LayerTrainFunction,
