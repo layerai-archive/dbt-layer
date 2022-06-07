1df2da869ab0a7edda52c748d5ab337e13c07afb
Burak Ozen <burak@layer.co>
2022-06-07 15:04:53 +0200

    Update predictions.sql

--- examples/order_review_prediction/models/predictions.sql
+++ examples/order_review_prediction/models/predictions.sql
@@ -1,6 +1,6 @@
 SELECT
        order_id,
        review_score,
-       layer.predict("layer/ecommerce_olist_order_review_score_prediction/models/review_score_predictor_model", ARRAY[days_between_purchase_and_delivery, order_approved_late, actual_delivery_vs_expectation_bucket, actual_delivery_vs_expectation_bucket, total_order_freight, is_multiItems_order, seller_shipped_late]) as review_prediction
+       layer.predict("layer/ecommerce_olist_order_review_score_prediction/models/review_score_predictor_model", ARRAY[days_between_purchase_and_delivery, order_approved_late, actual_delivery_vs_expectation_bucket, total_order_price, total_order_freight, is_multiItems_order, seller_shipped_late]) as review_prediction
 FROM
      {{ref('training_data')}}
