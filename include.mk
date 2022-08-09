28771c662cfed9d1cca5405a18fc01e7f555bd57
Michel Lebeau <github.com@mchl.app>
2022-08-09 08:33:47 +0100

    Port over sdk logic for python virtual envs (#127)

---
+++ include.mk
@@ -0,0 +1,9 @@
+INSTALL_STAMP := .install.stamp
+POETRY := $(shell command -v poetry 2> /dev/null)
+EXTRAS ?= "bigquery snowflake"
+IN_VENV := $(shell echo $(CONDA_DEFAULT_ENV)$(CONDA_PREFIX)$(VIRTUAL_ENV))
+REQUIRED_POETRY_VERSION := 1.1.14
+UNAME_SYS := $(shell uname -s)
+UNAME_ARCH := $(shell uname -m)
+PROJECT_NAME := dbt-layer
+CONDA_ENV_NAME := $(shell echo $(CONDA_DEFAULT_ENV))
