caa99e59cbf2521c71f87395a113b6f9a8e78df4
Dimitar Popov <dimitar@layer.co>
2022-09-06 16:52:15 +0100

    Poetry 1.2.0

--- include.mk
+++ include.mk
@@ -2,12 +2,12 @@ INSTALL_STAMP := .install.stamp
 POETRY := $(shell command -v poetry 2> /dev/null)
 EXTRAS ?= "bigquery snowflake"
 IN_VENV := $(shell echo $(CONDA_DEFAULT_ENV)$(CONDA_PREFIX)$(VIRTUAL_ENV))
-REQUIRED_POETRY_VERSION := 1.1.15
+REQUIRED_POETRY_VERSION := 1.2.0
 UNAME_SYS := $(shell uname -s)
 UNAME_ARCH := $(shell uname -m)
 PROJECT_NAME := dbt-layer
 CONDA_ENV_NAME := $(shell echo $(CONDA_DEFAULT_ENV))
 
-ifneq ($(shell $(POETRY) --version | awk '{print $$3}'), $(REQUIRED_POETRY_VERSION))
+ifneq ($(shell $(POETRY) --version | sed -En 's/Poetry \(version (.*)\)/\1/p'), $(REQUIRED_POETRY_VERSION))
 $(error "Please use Poetry version $(REQUIRED_POETRY_VERSION). Simply run: poetry self update $(REQUIRED_POETRY_VERSION)")
 endif
